86938bf9b613a856bd3272df914b91e620dc2dee
Mariangela Salcedo <mariangelasalcedo@ballastlane.com>
2018-07-31 10:05:12 -0400

    Updated library: Added optional dialog preview in permission activity

--- baselibrary/src/main/java/com/ballastlane/android/baselibrary/common/base/BasePermissionActivity.kt
+++ baselibrary/src/main/java/com/ballastlane/android/baselibrary/common/base/BasePermissionActivity.kt
@@ -1,13 +1,18 @@
 package com.ballastlane.android.baselibrary.common.base
 
+import android.content.Context
+import android.content.DialogInterface
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.util.Log
+import android.view.Gravity
+import android.widget.TextView
 import com.ballastlane.android.baselibrary.R
 import com.tbruyelle.rxpermissions2.RxPermissions
-import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
 import io.reactivex.subjects.PublishSubject
+
 
 /**
  * Created by Mariangela Salcedo (mariangelasalcedo@ballastlane.com) on 3/8/18.
@@ -19,20 +24,56 @@ abstract class BasePermissionActivity : BaseActivity() {
 
     abstract val required: Boolean
 
+    abstract val dialogPreview: DialogPreview?
+
     private val TAG = "TAG_${BasePermissionActivity::class.java.simpleName}"
 
     private val acceptedPermissionsSubject = PublishSubject.create<Any>()
     private val rxPermissions by lazy { RxPermissions(this) }
 
     var requestDisposable: Disposable? = null
-    val acceptedPermissionsObservable: Observable<Any> = acceptedPermissionsSubject
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        request()
+
+        if (dialogPreview != null && !hasPermission()) {
+            dialogPreview()
+        } else {
+            request()
+        }
+    }
+
+    private fun hasPermission(): Boolean {
+        var checkVal = true
+        permissions.forEach {
+            checkVal = (checkCallingOrSelfPermission(it) == PackageManager.PERMISSION_GRANTED) && checkVal
+        }
+        return checkVal
+    }
+
+    private fun dialogPreview() {
+        val alertDialog = AlertDialog.Builder(this).create()
+        alertDialog.setMessage(dialogPreview!!.message)
+        val title = TextView(this)
+        title.text = dialogPreview!!.title
+        title.gravity = Gravity.CENTER
+        title.setPadding(60, 80, 60, 10)
+        title.textSize = 18F
+        alertDialog.setCancelable(false)
+        alertDialog.setCustomTitle(title)
+        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, dialogPreview!!.buttonText, { dialog, _ ->
+            request()
+            dialog.dismiss()
+        })
+
+        alertDialog.show()
+
+        val mw = alertDialog.findViewById<TextView>(android.R.id.message)
+        mw!!.gravity = Gravity.CENTER
     }
 
     private fun request() {
+
         Log.d(TAG, "request permissions")
         requestDisposable?.dispose()
         requestDisposable = rxPermissions.request(*permissions)
@@ -69,5 +110,12 @@ abstract class BasePermissionActivity : BaseActivity() {
             })
         }
         builder.show()
+    }
+
+    class DialogPreview(val title: String, val message: String, val buttonText: String) {
+        constructor(titleRes: Int, messageRes: Int, buttonTextRes: Int, context: Context) :
+                this(context.resources.getString(titleRes),
+                        context.resources.getString(messageRes),
+                        context.resources.getString(buttonTextRes))
     }
 }
