26fce3a37927a314c7da9ef1d06f6ee6dd7640b8
Mariangela Salcedo <mariangelasalcedo@ballastlane.com>
2018-09-18 07:19:32 -0500

    Updated library

--- sample/src/main/java/com/ballastlane/ballastlanebaseapp/SampleApp.kt
+++ sample/src/main/java/com/ballastlane/ballastlanebaseapp/SampleApp.kt
@@ -8,6 +8,13 @@ import com.ballastlane.android.baselibrary.app.BaseApp
  * Copyright (c) 2018 Ballast Lane Applications LLC. All rights reserved.
  */
 open class SampleApp : BaseApp() {
+    override fun onForeground() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBackground() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     private val TAG = "TAG_${SampleApp::class.java.simpleName}"
 
